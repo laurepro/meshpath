f8cce4f6f116b150ba367abce94fe01207c2145e
laurepro <laurent.prott@gmail.com>
2023-08-28 12:30:38 +0200

    meshes

--- meshpath.js
+++ meshpath.js
@@ -1,3 +1,25 @@
+class History {
+  constructor() {
+    this.list = [];
+    this.index = 0;
+  }
+  add() {
+    this.index++;
+    let portion = this.list.slice(0, this.index + 1);
+    let ajout = [];
+    Array.from(arguments).forEach((a) => ajout.push(JSON.stringify(a)));
+    portion.push(ajout);
+    this.list = portion;
+  }
+  go(sens) {
+    this.index += sens;
+    this.index = Math.min(Math.max(this.index, 1), this.list.length);
+    let extract = [];
+    this.list[this.index - 1].forEach((i) => extract.push(JSON.parse(i)));
+    return extract;
+  }
+}
+
 window.addEventListener("load", (le) => {
   const tools = document.querySelector("body>div");
   document.addEventListener("keyup", (ke) => {
@@ -21,6 +43,7 @@ window.addEventListener("load", (le) => {
                 ls.forEach((l) => layer.push(l));
                 svg.initialize();
                 savelayer();
+                historize();
               }
               var img = inputfile.querySelector("image#background");
               if (img) {
@@ -30,6 +53,7 @@ window.addEventListener("load", (le) => {
                   image: img.getAttribute("href"),
                 };
                 savebackground();
+                historize();
                 image.initialize();
               }
             default:
@@ -42,6 +66,7 @@ window.addEventListener("load", (le) => {
                   image: re.target.result,
                 };
                 savebackground();
+                historize();
                 image.initialize();
               };
           }
@@ -54,46 +79,70 @@ window.addEventListener("load", (le) => {
       }
     }
   });
-  tools.querySelector("button.add").addEventListener("click", (ce) => {
+  tools.querySelector("button.add.layer").addEventListener("click", (ce) => {
     let newid = layer.length;
-    layer[newid] = [...layer[layer.length - 1]] || [];
+    layer[newid] = JSON.parse(JSON.stringify(layer[layer.length - 1]));
     savelayer();
+    historize();
     tools.addlayer(newid);
     drawsvg();
   });
+  tools.querySelector("button.add.group").addEventListener("click", (ce) => {
+    tools.addgroup(layer[curlayer()].length);
+    for (let i = 0; i < layer.length; i++) {
+      layer[i].push([]);
+    }
+    savelayer();
+    historize();
+  });
+  tools.addgroup = (id) => {
+    let newgroup = document.createElement("label");
+    newgroup.setAttribute("group", id);
+    newgroup.innerHTML = `<input name="group" type="radio" checked><div>${id}</div>`;
+    tools.insertBefore(newgroup, tools.querySelector("button.add.group"));
+  };
   tools.addlayer = (id) => {
     let newlayer = document.createElement("label");
     newlayer.setAttribute("layer", id);
     newlayer.innerHTML = `<input name="layer" type="radio" checked><div>${id}</div>`;
-    tools.insertBefore(newlayer, tools.querySelector("button.add"));
+    tools.insertBefore(newlayer, tools.querySelector("button.add.layer"));
   };
   tools.addEventListener("click", (ce) => {
     if (ce.target.tagName == "INPUT") {
       if (ce.target.name == "layer") {
+        layers.querySelector("path.active").classList.remove("active");
         hooks.querySelector("g.active").classList.remove("active");
+        layers.querySelector(`path[layer="${ce.target.parentElement.getAttribute("layer")}"]`).classList.add('active');
         let active = hooks.querySelector(`g[layer="${ce.target.parentElement.getAttribute("layer")}"]`);
         active.classList.add("active");
         hooks.appendChild(active);
-      }
-      else if (ce.target.name == "mesh") {
-        meshes.style.display = ce.target.checked ? 'initial' : '';
+      } else if (ce.target.name == "mesh") {
+        meshes.style.display = ce.target.checked ? "initial" : "";
       }
     }
   });
   tools.querySelector("button.clear").addEventListener("click", (ce) => {
     if (confirm("sure ?")) {
-      layer = [[[]]];
-      background = {};
-      layers.querySelector('path[layer="0"]').setAttribute("d", "");
-      hooks.querySelector('g[layer="0"]').innerHTML = "";
-      layers.querySelectorAll('path:not([layer="0"])').forEach((p) => p.remove());
-      hooks.querySelectorAll('g:not([layer="0"])').forEach((p) => p.remove());
-      tools.querySelectorAll('label[layer]:not([layer="0"])').forEach((p) => p.remove());
-      tools.querySelector('label[layer="0"]>input').checked = true;
-      savebackground();
-      savelayer();
-      svg.initialize();
+      localStorage.removeItem("layer");
+      localStorage.removeItem("background");
+      location.reload();
     }
+  });
+  tools.querySelector("button.undo").addEventListener("click", (ce) => {
+    ret = history.go(-1);
+    layer = ret[0];
+    background = ret[1];
+    savelayer();
+    savebackground();
+    drawsvg();
+  });
+  tools.querySelector("button.redo").addEventListener("click", (ce) => {
+    ret = history.go(1);
+    layer = ret[0];
+    background = ret[1];
+    savelayer();
+    savebackground();
+    drawsvg();
   });
   tools.querySelector("button.save").addEventListener("click", (ce) => {
     const rect = svg.getBoundingClientRect();
@@ -128,36 +177,45 @@ ${image.outerHTML}
   });
 
   const curlayer = () => tools.querySelector('input[name="layer"]:checked').parentElement.getAttribute("layer");
+  const curgroup = () => tools.querySelector('input[name="group"]:checked').parentElement.getAttribute("group");
   const drawpathall = () => layer.map((l, k) => drawpath(l, k)).join("");
-  const drawpath = (l, k) => `<path layer="${k}" d="${bezierPath(l.map((p) => [p.x, p.y]))}" stroke="black" fill="none"/>`;
+  const drawpath = (l, k) => `<path layer="${k}" class="${k == curlayer() ? "active" : ""}" d="${l.map((g) => drawgroup(g)).join(" ")}" stroke="black" fill="none"/>`;
+  const drawgroup = (g) => bezierPath(g.map((p) => [p.x, p.y]));
   const drawpointall = () => layer.map((l, k) => drawpoint(l, k)).join("");
-  const drawpoint = (l, k) => `<g layer="${k}" class="${k == curlayer() ? "active" : ""}">${l.map((p) => `<circle cx="${p.x}" cy="${p.y}" r="3"/>`).join("")}$</g>`;
+  const drawpoint = (l, k) => `
+  <g layer="${k}" class="${k == curlayer() ? "active" : ""}">
+  ${l.map((g, k) => `<g group="${k}">${g.map((p) => `<circle cx="${p.x}" cy="${p.y}" r="3"/>`).join("")}</g>`).join("")}
+  </g>`;
   const drawmeshall = () => {
     let mesh = [];
-    for (var i = 0; i < layer[0].length; i++) {
-      mesh.push(drawmesh(i));
+    for (var g = 0; g < layer[0].length; g++) {
+      for (var i = 0; i < layer[0][g].length; i++) {
+        mesh.push(drawmesh(g, i));
+      }
     }
     return mesh.join("");
   };
-  const drawmesh = (i) => {
+  const drawmesh = (g, i) => {
     let path = [];
-    for (var m = 0; m < layer.length; m++) {
-      path.push(layer[m][i]);
+    for (var l = 0; l < layer.length; l++) {
+      path.push(layer[l][g][i]);
     }
-    return `<path mesh="${i}" d="${path.map((p, k) => `${k==0 ? 'M' : 'L'} ${p.x},${p.y}`)}" stroke="red" fill="none"/>`;
+    return `<path group="${g}" point="${i}" d="${path.map((p, k) => `${k == 0 ? "M" : "L"} ${p.x},${p.y}`)}" marker-end="url(#arrowhead)" stroke="red" fill="none"/>`;
   };
   const savelayer = () => localStorage.setItem("layer", JSON.stringify(layer));
   const savebackground = () => localStorage.setItem("background", JSON.stringify(background));
-  const loadlayer = () => JSON.parse(localStorage.getItem("layer") || "[[]]");
+  const loadlayer = () => JSON.parse(localStorage.getItem("layer") || "[[[]]]");
   const loadbackground = () => JSON.parse(localStorage.getItem("background") || "{}");
   const drawsvg = () => {
     layers.innerHTML = drawpathall();
     hooks.innerHTML = drawpointall();
     meshes.innerHTML = drawmeshall();
   };
+  const historize = () => history.add(layer, background);
 
   var layer = loadlayer();
   var background = loadbackground();
+  const history = new History();
   const svg = document.querySelector("body>svg");
   const layers = document.querySelector("body>svg>g#layers");
   const hooks = document.querySelector("body>svg>g#hooks");
@@ -165,51 +223,71 @@ ${image.outerHTML}
   const image = document.querySelector("body>svg>image#background");
   const animate = document.querySelector("body>svg>g#animate");
 
-  animate.start = () => animate.innerHTML = `<path stroke="blue" stroke-width="3" fill="none"><animate dur="${tools.querySelector('input[name="speed"]').value}s" repeatCount="indefinite" attributeName="d" values="${Array.from(layers.querySelectorAll('path')).map((p)=>p.getAttribute('d')).join('; ')}" /></path>`;
-  animate.stop = () => animate.innerHTML = "";
+  animate.start = () =>
+    (animate.innerHTML = `<path stroke="blue" stroke-width="3" fill="none"><animate dur="${tools.querySelector('input[name="speed"]').value}s" repeatCount="indefinite" attributeName="d" values="${Array.from(layers.querySelectorAll("path"))
+      .map((p) => p.getAttribute("d").trim() || "M 0,0")
+      .join("; ")}" /></path>`);
+  animate.stop = () => (animate.innerHTML = "");
 
   svg.trace = false;
+  svg.group = false;
   svg.move = false;
-  svg.join = false;
 
   document.addEventListener("mousedown", () => animate.stop());
+  svg.addEventListener("dblclick", (dc) => {
+    if (dc.target.tagName == "circle") {
+      tools.querySelector(`label[layer="${dc.target.parentNode.parentNode.getAttribute("layer")}"]>input`).click();
+    }
+  });
+
   svg.addEventListener("mousedown", (md) => {
-    if (md.ctrlKey) {
+    if (md.shiftKey) {
       if (md.target.tagName == "circle") {
         let index = [...md.target.parentNode.children].indexOf(md.target);
-        if (index == 0 || index == layer[curlayer()].length - 1) {
+        var group = md.target.parentNode.getAttribute("group");
+        if (index == 0 || index == layer[curlayer()][group].length - 1) {
           svg.trace = index == 0 ? -1 : 1;
-          svg.join = md.target;
+          svg.group = md.target.parentNode.getAttribute("group");
         }
-      }
-      else {
+      } else if (layer[curlayer()][curgroup()].length == 0) {
         svg.trace = true;
+        svg.group = false;
       }
     } else {
       if (md.target.tagName == "circle") {
-        if (md.shiftKey) {
-          tools.querySelector(`label[layer="${md.target.parentElement.getAttribute("layer")}"]>input`).click();
-        } else if (md.altKey) {
-          let index = [...md.target.parentNode.children].indexOf(md.target);
+        let group = md.target.parentNode;
+        if (md.ctrlKey) {
+          let index = [...group.children].indexOf(md.target);
+          let j = group.getAttribute("group");
           for (let i = 0; i < layer.length; i++) {
-            let points = [layer[i][index]];
+            let points = [layer[i][j][index]];
             if (index > 0) {
               points.unshift({
-                x: layer[i][index].x - (layer[i][index].x - layer[i][index - 1].x) / 2,
-                y: layer[i][index].y - (layer[i][index].y - layer[i][index - 1].y) / 2,
+                x: layer[i][j][index].x - (layer[i][j][index].x - layer[i][j][index - 1].x) / 2,
+                y: layer[i][j][index].y - (layer[i][j][index].y - layer[i][j][index - 1].y) / 2,
               });
             }
-            if (index < layer[i].length - 1) {
+            if (index < layer[i][j].length - 1) {
               points.push({
-                x: layer[i][index].x + (layer[i][index + 1].x - layer[i][index].x) / 2,
-                y: layer[i][index].y + (layer[i][index + 1].y - layer[i][index].y) / 2,
+                x: layer[i][j][index].x + (layer[i][j][index + 1].x - layer[i][j][index].x) / 2,
+                y: layer[i][j][index].y + (layer[i][j][index + 1].y - layer[i][j][index].y) / 2,
               });
             }
-            layer[i] = layer[i].slice(0, index).concat(points, layer[i].slice(index + 1));
+            layer[i][j] = layer[i][j].slice(0, index).concat(points, layer[i][j].slice(index + 1));
           }
           savelayer();
+          historize();
           drawsvg();
-        } else if (md.target.parentNode.getAttribute("layer") == curlayer()) {
+        } else if (md.altKey) {
+          let index = [...group.children].indexOf(md.target);
+          let j = group.getAttribute("group");
+          for (let i = 0; i < layer.length; i++) {
+            layer[i][j] = layer[i][j].slice(0, index).concat(layer[i][j].slice(index + 1));
+          }
+          savelayer();
+          historize();
+          drawsvg();
+        } else if (group.parentNode.getAttribute("layer") == curlayer()) {
           svg.move = md.target;
         }
       }
@@ -217,45 +295,55 @@ ${image.outerHTML}
   });
 
   svg.addEventListener("mouseup", (mu) => {
-    if (svg.trace !== false) {
-      let source = layer[curlayer()];
+    if (svg.trace !== false && mu.target.tagName == "circle") {
+      let g = mu.target.parentNode.getAttribute("group");
+      let l = mu.target.parentNode.parentNode.getAttribute("layer");
+
+      let source = layer[l][g];
       layer.forEach((v, k) => {
-        if (k != curlayer()) {
+        if (k != l) {
           if (svg.trace == 1) {
-            layer[k] = layer[k].concat(source.slice(layer[k].length));
+            layer[k][g] = layer[k][g].concat(source.slice(layer[k][g].length));
           } else {
-            layer[k] = source.slice(0, source.length - layer[k].length).concat(layer[k]);
+            layer[k][g] = source.slice(0, source.length - layer[k][g].length).concat(layer[k][g]);
           }
         }
       });
       savelayer();
       drawsvg();
     }
+    if (svg.trace || svg.move) {
+      historize();
+    }
     svg.trace = false;
+    svg.group = false;
     svg.move = false;
   });
 
   svg.addEventListener("mousemove", (me) => {
     if (me.buttons == 1) {
-      if (me.ctrlKey && svg.trace !== false) {
-        path = layer[curlayer()];
+      if (me.shiftKey && svg.trace !== false) {
+        var group = svg.group || curgroup();
+        path = layer[curlayer()][group];
         if (svg.trace == 1) {
           path.push({ x: me.x, y: me.y });
         } else {
           path.unshift({ x: me.x, y: me.y });
         }
         path = simplify(path);
-        layer[curlayer()] = path;
+        layer[curlayer()][group] = path;
         layers.querySelector(`path[layer="${curlayer()}"]`).outerHTML = drawpath(layer[curlayer()], curlayer());
         hooks.querySelector(`g[layer="${curlayer()}"]`).outerHTML = drawpoint(layer[curlayer()], curlayer());
         savelayer();
       } else if (svg.move !== false) {
-        let index = [...svg.move.parentNode.children].indexOf(svg.move);
-        layer[curlayer()][index] = { x: me.x, y: me.y };
+        let group = svg.move.parentNode;
+        let g = group.getAttribute("group");
+        let i = [...group.children].indexOf(svg.move);
+        layer[curlayer()][g][i] = { x: me.x, y: me.y };
         svg.move.setAttribute("cx", me.x);
         svg.move.setAttribute("cy", me.y);
         layers.querySelector(`path[layer="${curlayer()}"]`).outerHTML = drawpath(layer[curlayer()], curlayer());
-        meshes.querySelector(`path[mesh="${index}"]`).outerHTML = drawmesh(index);
+        meshes.querySelector(`path[group="${g}"][point="${i}"]`).outerHTML = drawmesh(g, i);
         savelayer();
       }
     }
@@ -277,9 +365,12 @@ ${image.outerHTML}
     for (let i = 0; i < layer.length; i++) {
       tools.addlayer(i);
     }
+    for (let i = 0; i < layer[0].length; i++) {
+      tools.addgroup(i);
+    }
     image.initialize();
     drawsvg();
   };
-
+  historize();
   svg.initialize();
 });
